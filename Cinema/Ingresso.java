c9575f89e5db43c633c7879f8f1d83daa6c8306d
Jonathas Nóbrega <57995489+JohnnyStalker@users.noreply.github.com>
2022-07-13 16:58:28 -0300

    Update Ingresso.java

--- Cinema/Ingresso.java
+++ Cinema/Ingresso.java
@@ -24,7 +24,37 @@ public class Ingresso {
 		this.valor = valor;
 	}
 
+	public String getData() {
+		return data;
+	}
 
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSala() {
+		return sala;
+	}
+
+	public void setSala(String sala) {
+		this.sala = sala;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
 
 	public double imprimeValor(){
 		return getValor();
