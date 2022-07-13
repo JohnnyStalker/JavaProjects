fbf6adcdd28678684f19dc392680e312263b3650
Jonathas Nóbrega <57995489+JohnnyStalker@users.noreply.github.com>
2022-07-13 16:55:10 -0300

    Update Cinema.java

--- Cinema/Cinema.java
+++ Cinema/Cinema.java
@@ -6,12 +6,40 @@ import java.util.ArrayList;
 public class Cinema {
 	 private ArrayList<Cliente> Clientes;
 
-	public Cinema(ArrayList<Cliente> clientes) {
+	public Cinema() {
 		super();
 		this.Clientes = new ArrayList<Cliente>();
 	}
-	 
+
+	public ArrayList<Cliente> getClientes() {
+		return Clientes;
+	}
+	
+	public void setClientes(ArrayList<Cliente> clientes) {
+		Clientes = clientes;
+	}
+
 	public void addCliente(Cliente cliente) {
-		Clientes.equals(cliente);
+		if (Clientes.contains(cliente)) {
+			System.out.println("\nCliente já faz parte da lista!");
+		}
+		else {
+			Clientes.add(cliente);
+			System.out.println("\nCliente adicionado com sucesso!");
+		}
+	}
+	
+	public void removerCliente(Cliente cliente) {
+		if (Clientes.contains(cliente)) {
+			Clientes.remove(cliente);
+			System.out.println("\nCliente removido com sucesso!");
+		}
+		else {
+			System.out.println("\nO Cliente inserido não está cadastrado!");
+		}
+	}
+	
+	public void imprimirClientes() {
+		Clientes.forEach(cliente -> System.out.println(cliente));
 	}
 }
