e9b0c5ba2b5893b310579a528462da11e807dc0c
Jonathas Nóbrega <57995489+JohnnyStalker@users.noreply.github.com>
2022-07-13 16:56:39 -0300

    Update Cliente.java

--- Cinema/Cliente.java
+++ Cinema/Cliente.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Cliente {
 	Scanner sc = new Scanner(System.in);
 	private int op = 0;
+
 	private String nome;
 	private int cpf;
 	private String card;
@@ -23,7 +24,10 @@ public class Cliente {
 		this.card = card;
 		this.ingressosC = ingressosC;
 	}
-
+	
+	public String getNome() {
+		return nome;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -42,18 +46,23 @@ public class Cliente {
 		op = sc.nextInt();
 		switch (op) {
 		case 1:
-			System.out.println("\nIngresso comum comprado com sucesso!\n");
+			System.out.println("\nIngresso comum comprado com sucesso!");
 			break;
 		case 2:
-			System.out.println("\nIngresso Vip Gold comprado com sucesso!\n");
+			System.out.println("\nIngresso Vip Gold comprado com sucesso!");
 			break;
 		case 3:
-			System.out.println("\nIngresso Vip Platinum comprado com sucesso!\n");
+			System.out.println("\nIngresso Vip Platinum comprado com sucesso!");
 			break;
 		default:
-			System.out.println("\nNão há ingresso com esse valor digitado!\n");
+			System.out.println("\nNão há ingresso com a opção digitada!");
 			break;
 		}
+	}
+	
+	@Override
+	public String toString() {
+		return "\n=-=-=Cliente=-=-=\n" + "Nome: " + nome + "\nCPF: " + cpf + "\nIngressos Comprados: " + ingressosC;
 	}
 	
 }
