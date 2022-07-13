605037488130ed876418e5cf338e05e58c9c169d
Jonathas Nóbrega <57995489+JohnnyStalker@users.noreply.github.com>
2022-07-13 20:13:43 -0300

    Add files via upload

--- Cinema/Ingresso.java
+++ Cinema/Ingresso.java
@@ -19,41 +19,21 @@ public class Ingresso {
 	public double getValor() {
 		return valor;
 	}
-
-	public void setValor(double valor) {
-		this.valor = valor;
-	}
-
+	
 	public String getData() {
 		return data;
-	}
-
-	public void setData(String data) {
-		this.data = data;
 	}
 
 	public String getNome() {
 		return nome;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 	public String getSala() {
 		return sala;
 	}
 
-	public void setSala(String sala) {
-		this.sala = sala;
-	}
-
 	public String getHora() {
 		return hora;
-	}
-
-	public void setHora(String hora) {
-		this.hora = hora;
 	}
 
 	public double imprimeValor(){
