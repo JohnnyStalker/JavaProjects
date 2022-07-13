1022a3e076e76607eadc506f3f9402199935baf5
Jonathas Nóbrega <57995489+JohnnyStalker@users.noreply.github.com>
2022-07-12 22:23:30 -0300

    Create Cinema.java

---
+++ Cinema/Cinema.java
@@ -0,0 +1,17 @@
+package Cinema;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Cinema {
+	 private ArrayList<Cliente> Clientes;
+
+	public Cinema(ArrayList<Cliente> clientes) {
+		super();
+		this.Clientes = new ArrayList<Cliente>();
+	}
+	 
+	public void addCliente(Cliente cliente) {
+		Clientes.equals(cliente);
+	}
+}
